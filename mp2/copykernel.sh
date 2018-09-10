3ea2d38408bffa6d874ccf42cd29d12fa818045b
410 <guest@TA-virtualbox.(none)>
2018-09-09 23:22:36 -0500

    gdb changes

--- mp2/copykernel.sh
+++ mp2/copykernel.sh
@@ -1,5 +1,5 @@
 # Replace "/mnt/floppy" with the whatever directory is appropriate.
 sudo mount -o loop dev_kernel_grub.img /mnt/floppy
-sudo cp kernel.bin /mnt/floppy
+sudo cp kernel.elf /mnt/floppy
 sleep 1s
 sudo umount /mnt/floppy
