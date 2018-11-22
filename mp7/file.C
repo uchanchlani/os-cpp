3e24b6d57b55df41bb0d865f27e465cf03d77077
utkarshc <utkarshc@tamu.edu>
2018-11-22 15:06:32 -0600

    one shot implementation. It generally never works but let's see

--- mp7/file.C
+++ mp7/file.C
@@ -26,11 +26,17 @@
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-File::File() {
+File::File(unsigned short _startBlock, unsigned char * _blockBuf, FileSystem * _fileSystem) {
     /* We will need some arguments for the constructor, maybe pointer to disk
      block with file management and allocation data. */
     Console::puts("In file constructor.\n");
-    assert(false);
+    fileSystem = _fileSystem;
+    startBlock = _startBlock;
+    offset = 0;
+    blockNum = _startBlock;
+    for(unsigned short i = 0; i < 512; i++) {
+        blockBuf[i] = _blockBuf[i];
+    }
 }
 
 /*--------------------------------------------------------------------------*/
@@ -39,28 +45,151 @@ File::File() {
 
 int File::Read(unsigned int _n, char * _buf) {
     Console::puts("reading from file\n");
-    assert(false);
+    if(readMode ^ writeMode) {
+        readMode = true;
+        writeMode = false;
+    } else if (!readMode) {
+        Console::puts("Please don't write and read on the file simultaneously. It is not good you know.\n")
+        return 0;
+    }
+    if(_n == 0 || blockBuf[offset % 510] == 0)
+        return 0;
+    unsigned int charRead = 0;
+    while(_n > 0) {
+        _buf[charRead] = blockBuf[offset % 510];
+        if(_buf[charRead] == 0) {
+            return charRead;
+        }
+        charRead++;
+        offset++;
+        _n--;
+        if(offset % 510 == 0) {
+            unsigned short nextBlock = getNextBlock();
+            if (nextBlock != 0) {
+                fileSystem->disk->read(nextBlock, blockBuf);
+            } else {
+                blockBuf[0] = 0; // eof, so that next time if someone tries to read it they get this eof
+            }
+        }
+    }
 }
 
 
 void File::Write(unsigned int _n, const char * _buf) {
     Console::puts("writing to file\n");
-    assert(false);
+    if(readMode ^ writeMode) {
+        readMode = false;
+        writeMode = true;
+    } else if (!writeMode) {
+        Console::puts("Please don't write and read on the file simultaneously. It is not good you know.\n")
+    }
+    if(_n == 0 || blockBuf[offset % 510] == 0)
+        return;
+    unsigned int charRead = 0;
+    while(_n > 0) {
+        blockBuf[offset % 510] = _buf[charRead];
+        if(_buf[charRead] == 0) {
+            break;
+        }
+        charRead++;
+        offset++;
+        _n--;
+        if(offset % 510 == 0 && _n > 0) {
+            generateNewEmptyBlock();
+        }
+    }
+    flushBlock();
 }
 
 void File::Reset() {
     Console::puts("reset current position in file\n");
-    assert(false);
-    
+    offset = 0;
+    if (blockNum != startBlock) {
+        blockNum = startBlock;
+        fileSystem->disk->read(blockNum, blockBuf);
+    }
 }
 
 void File::Rewrite() {
     Console::puts("erase content of file\n");
-    assert(false);
+    if(readMode ^ writeMode) {
+        readMode = false;
+        writeMode = true;
+    } else if (!writeMode) {
+        Console::puts("Please don't write and read on the file simultaneously. It is not good you know.\n")
+    }
+    bool isStale = returnBlocks(true);
+    blockNum = startBlock;
+    if(isStale)
+        fileSystem->disk->read(blockNum, blockBuf);
+    blockBuf[0] = 0;
+    if(getNextBlock() != 0) {
+        setNextBlock(0);
+        flushBlock();
+    }
 }
 
 
 bool File::EoF() {
     Console::puts("testing end-of-file condition\n");
-    assert(false);
+    return blockBuf[offset % 510] == 0;
 }
+
+void File::generateNewEmptyBlock() {
+    if(offset % 510 != 0) {
+        // it should never reach here
+        assert(false);
+    }
+    unsigned char tempBuf[512];
+    unsigned short _blockNum = fileSystem->getFreeBlock(tempBuf);
+    setNextBlock(_blockNum);
+    flushBlock();
+    resetFileBits(blockBuf);
+    blockNum = _blockNum;
+//    flushBlock();
+}
+
+void File::resetFileBits(unsigned char *buf) {
+    for(unsigned short i = 0; i < 512; i++)
+        buf[i] = 0;
+}
+
+void File::flushBlock() {
+    fileSystem->disk->write((unsigned long)blockBuf, blockBuf);
+}
+
+bool File::returnBlocks(bool keepFirst) {
+    unsigned char tempBuf[512];
+    bool isStale = false;
+    unsigned short retBlocks[100];
+    unsigned long sizeRetBlocks = 0;
+    Reset();
+    if(keepFirst) {
+        blockNum = getNextBlock();
+        isStale = blockNum > 0;
+    }
+    while(blockNum != 0) {
+        if(isStale) {
+            fileSystem->disk->read(blockNum, blockBuf);
+        }
+        retBlocks[sizeRetBlocks++] = blockNum;
+        blockNum = getNextBlock();
+        isStale = true;
+        if(sizeRetBlocks == 100) {
+            fileSystem->returnFreeBlock(tempBuf, retBlocks, sizeRetBlocks);
+        }
+    }
+    if(sizeRetBlocks > 0) {
+        fileSystem->returnFreeBlock(tempBuf, retBlocks, sizeRetBlocks);
+    }
+    return isStale;
+}
+
+unsigned short File::getNextBlock() {
+    return *((unsigned short *)(blockBuf + 510));
+}
+
+void File::setNextBlock(unsigned short _blockNum) {
+    *((unsigned short *)(blockBuf+510)) = _blockNum;
+}
+
--- mp7/file_system.C
+++ mp7/file_system.C
@@ -12,7 +12,11 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+typedef unsigned int size_t;
+extern void * operator new (size_t size);
+extern void * operator new[] (size_t size);
+extern void operator delete (void * p);
+extern void operator delete[] (void * p);
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -28,35 +32,221 @@
 /*--------------------------------------------------------------------------*/
 
 FileSystem::FileSystem() {
-    Console::puts("In file system constructor.\n");
-    assert(false);
+    disk = NULL;
+    isValid = false;
 }
 
 /*--------------------------------------------------------------------------*/
 /* FILE SYSTEM FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
+void FileSystem::readMeta(unsigned char *usableBuf) {
+    this->disk->read(0, usableBuf);
+    FileSystemMeta * _meta = (FileSystemMeta *) usableBuf;
+    meta.freeListBlock = _meta->freeListBlock;
+    meta.iNodeListBlock = _meta->iNodeListBlock;
+    meta.signature = _meta->signature;
+    meta.start_data_block = _meta->start_data_block;
+    this->isValid = this->meta.signature == FS_SIGNATURE;
+}
+
+void FileSystem::updateMeta(unsigned char *usableBuf) {
+    unsigned char * _buf = (unsigned char *) &meta;
+    for(unsigned short i = 0; i < sizeof(FileSystemMeta); i++) {
+        usableBuf[i] = _buf[i];
+    }
+    this->disk->write(0, usableBuf);
+}
+
+void FileSystem::updateFreeBlocksFromBuf(unsigned char * buf) {
+    unsigned short * _free_blocks = (unsigned short *) buf;
+    for(unsigned short i = 0; i < FREE_BLOCKS_COUNT; i++) {
+        free_blocks[i] = _free_blocks[i];
+    }
+}
+
+void FileSystem::castFreeBlockToBuf(unsigned char *buf) {
+    unsigned char * _buf = (unsigned char *) free_blocks;
+    for(unsigned short i = 0; i < FREE_BLOCKS_COUNT * sizeof(unsigned short); i++) {
+        buf[i] = _buf[i];
+    }
+}
+
+void FileSystem::updateINodesFromBuf(unsigned char *buf) {
+    INode * _inodes = (INode *) buf;
+    for(unsigned short i = 0; i < INODES_COUNT; i++) {
+        inodes[i].fileName = _inodes[i].fileName;
+        inodes[i].startBlock = _inodes[i].startBlock;
+    }
+}
+
+void FileSystem::castINodesToBuf(unsigned char *buf) {
+    unsigned char * _buf = (unsigned char *) inodes;
+    for(unsigned short i = 0; i < INODES_COUNT * sizeof(INode); i++) {
+        buf[i] = _buf[i];
+    }
+}
+
+unsigned short FileSystem::getFreeBlock(unsigned char * usableBuf) {
+    for(unsigned short i = FREE_BLOCKS_COUNT - 1; i > 0; i--) {
+        if(free_blocks[i] != 0) {
+            unsigned short block = free_blocks[i];
+            free_blocks[i] = 0;
+            updateFreeBlocksToDisk(usableBuf);
+            return block;
+        }
+    }
+    unsigned short candidate = free_blocks[0];
+    if(candidate != 0) {
+        this->disk->read(candidate, usableBuf);
+        updateFreeBlocksFromBuf(usableBuf);
+        updateFreeBlocksToDisk(usableBuf);
+        return candidate;
+    } else {
+        return 0;
+    }
+}
+
+bool FileSystem::returnFreeBlock(unsigned char * usableBuf, unsigned short * block_nums, unsigned long size) {
+    while (size > 0) {
+        for (unsigned short i = 1; i < FREE_BLOCKS_COUNT; i++ && size > 0) {
+            if (free_blocks[i] == 0) {
+                free_blocks[i] = *block_nums;
+                size--;
+                block_nums++;
+            }
+        }
+        if(size > 0) {
+            unsigned short candidate = *block_nums;
+            size--;
+            block_nums++;
+            updateFreeBlocksToDisk(usableBuf);
+            refreshFreeBlocks();
+            free_blocks[0] = candidate;
+        }
+    }
+    updateFreeBlocksToDisk(usableBuf);
+    return true;
+}
+
+void FileSystem::updateFreeBlocksToDisk(unsigned char * usableBuf) {
+    castFreeBlockToBuf(usableBuf);
+    this->disk->write(this->meta.freeListBlock, usableBuf);
+}
+
+void FileSystem::updateINodesToDisk(unsigned char * usableBuf) {
+    castINodesToBuf(usableBuf);
+    this->disk->write(this->meta.iNodeListBlock, usableBuf);
+}
+
+void FileSystem::refreshFreeBlocks() {
+    for(unsigned short i = 0; i < FREE_BLOCKS_COUNT; i++) {
+        free_blocks[i] = 0;
+    }
+}
+
+void FileSystem::clearINodes() {
+    for(unsigned short i = 0; i < INODES_COUNT; i++) {
+        inodes[i].fileName = 0;
+    }
+}
+
 bool FileSystem::Mount(SimpleDisk * _disk) {
-    Console::puts("mounting file system form disk\n");
-    assert(false);
+    disk = _disk;
+    unsigned char[512] buf;
+    readMeta(buf);
+    if(!this->isValid) {
+        return false;
+    }
+
+    this->disk->read(this->meta.iNodeListBlock, buf);
+    updateINodesFromBuf(buf);
+    this->disk->read(this->meta.freeListBlock, buf);
+    updateFreeBlocksFromBuf(buf);
+    return true;
 }
 
 bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) {
     Console::puts("formatting disk\n");
-    assert(false);
+    unsigned char buf[512];
+    FileSystem tempFileSystem;
+    tempFileSystem.disk = _disk;
+    tempFileSystem.isValid = false;
+    tempFileSystem.meta.size_in_blocks = (unsigned short)(_size / 512);
+    tempFileSystem.meta.signature = FS_SIGNATURE;
+    tempFileSystem.meta.freeListBlock = 1;
+    tempFileSystem.meta.iNodeListBlock = 2;
+    tempFileSystem.meta.start_data_block = 3;
+
+    tempFileSystem.clearINodes();
+    tempFileSystem.updateINodesToDisk(buf);
+
+    tempFileSystem.refreshFreeBlocks();
+    unsigned short * allBlocks = (unsigned short *) new unsigned short[tempFileSystem.meta.size_in_blocks];
+    for (unsigned short i = 0; i < tempFileSystem.meta.size_in_blocks; i++) {
+        allBlocks[i] = i;
+    }
+    tempFileSystem.returnFreeBlock(buf, allBlocks + tempFileSystem.meta.start_data_block,
+            tempFileSystem.meta.size_in_blocks - tempFileSystem.meta.start_data_block);
+
+    tempFileSystem.updateMeta(buf);
+    return true;
 }
 
 File * FileSystem::LookupFile(int _file_id) {
     Console::puts("looking up file\n");
-    assert(false);
+    if (!isValid) return NULL;
+    for(unsigned short i = 0; i < INODES_COUNT; i++) {
+        if(inodes[i].fileName == _file_id) {
+            unsigned char tempBuf[512];
+            disk->read(inodes[i].startBlock, tempBuf);
+            return new File(inodes[i].startBlock, tempBuf, this);
+        }
+    }
+    return NULL;
 }
 
 bool FileSystem::CreateFile(int _file_id) {
     Console::puts("creating file\n");
-    assert(false);
+    unsigned char tempBuf[512];
+    if (!isValid) return false;
+    File *pFile = LookupFile(_file_id);
+    if(pFile != NULL) {
+        Console::puts("File Already exists.\n");
+        delete pFile;
+        return false;
+    }
+    unsigned short _blockNum = this->getFreeBlock(tempBuf);
+    File::resetFileBits(tempBuf);
+    disk->write(_blockNum, tempBuf);
+    for(unsigned short i = 0; i < INODES_COUNT; i++) {
+        if(inodes[i].fileName == 0) {
+            inodes[i].fileName = (unsigned char)_file_id;
+            inodes[i].startBlock = _blockNum;
+            updateINodesToDisk(tempBuf);
+            return true;
+        }
+    }
+    return false;
 }
 
 bool FileSystem::DeleteFile(int _file_id) {
     Console::puts("deleting file\n");
-    assert(false);
+    unsigned char tempBuf[512];
+    if (!isValid) return false;
+    File *pFile = LookupFile(_file_id);
+    if (pFile == NULL) {
+        Console::puts("File does not exists.\n");
+        return false;
+    }
+    pFile->returnBlocks(false);
+    for (unsigned short i = 0; i < INODES_COUNT; i++) {
+        if (inodes[i].fileName == _file_id) {
+            inodes[i].fileName = 0;
+            updateINodesToDisk(tempBuf);
+            break;
+        }
+    }
+    delete pFile;
+    return true;
 }
